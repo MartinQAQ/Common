23abe61b6869f0bd6e93ef969daa7fc8752204bc
MartinQAQ <xiaoming723@126.com>
2020-06-09 22:20:51 +0800

    Create BaseMultiItemQuickAdapter.java

---
+++ Common/src/main/java/com/z_martin/common/common/adapterHelper/BaseMultiItemQuickAdapter.java
@@ -0,0 +1,131 @@
+package com.z_martin.common.common.adapterHelper;
+
+import android.util.SparseIntArray;
+import android.view.ViewGroup;
+
+import androidx.annotation.IntRange;
+import androidx.annotation.LayoutRes;
+
+import com.z_martin.common.common.adapterHelper.entity.IExpandable;
+import com.z_martin.common.common.adapterHelper.entity.MultiItemEntity;
+
+import java.util.List;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public abstract class BaseMultiItemQuickAdapter<T extends MultiItemEntity, K extends BaseViewHolder> extends BaseQuickAdapter<T, K> {
+
+    /**
+     * layouts indexed with their types
+     */
+    private SparseIntArray layouts;
+
+    private static final int DEFAULT_VIEW_TYPE = -0xff;
+    public static final int TYPE_NOT_FOUND = -404;
+
+    /**
+     * Same as QuickAdapter#QuickAdapter(Context,int) but with
+     * some initialization data.
+     *
+     * @param data A new list is created out of this one to avoid mutable list
+     */
+    public BaseMultiItemQuickAdapter(List<T> data) {
+        super(data);
+    }
+
+    @Override
+    protected int getDefItemViewType(int position) {
+        T item = mData.get(position);
+        if (item != null) {
+            return item.getItemType();
+        }
+        return DEFAULT_VIEW_TYPE;
+    }
+
+    protected void setDefaultViewTypeLayout(@LayoutRes int layoutResId) {
+        addItemType(DEFAULT_VIEW_TYPE, layoutResId);
+    }
+
+    @Override
+    protected K onCreateDefViewHolder(ViewGroup parent, int viewType) {
+        return createBaseViewHolder(parent, getLayoutId(viewType));
+    }
+
+    private int getLayoutId(int viewType) {
+        return layouts.get(viewType, TYPE_NOT_FOUND);
+    }
+
+    protected void addItemType(int type, @LayoutRes int layoutResId) {
+        if (layouts == null) {
+            layouts = new SparseIntArray();
+        }
+        layouts.put(type, layoutResId);
+    }
+
+
+    @Override
+    public void remove(@IntRange(from = 0L) int position) {
+        if (mData == null
+                || position < 0
+                || position >= mData.size()) return;
+
+        T entity = mData.get(position);
+        if (entity instanceof IExpandable) {
+            removeAllChild((IExpandable) entity, position);
+        }
+        removeDataFromParent(entity);
+        super.remove(position);
+    }
+
+    protected void removeAllChild(IExpandable parent, int parentPosition) {
+        if (parent.isExpanded()) {
+            List<MultiItemEntity> chidChilds = parent.getSubItems();
+            if (chidChilds == null || chidChilds.size() == 0) return;
+
+            int childSize = chidChilds.size();
+            for (int i = 0; i < childSize; i++) {
+                remove(parentPosition + 1);
+            }
+        }
+    }
+
+    protected void removeDataFromParent(T child) {
+        int position = getParentPosition(child);
+        if (position >= 0) {
+            IExpandable parent = (IExpandable) mData.get(position);
+            if (parent != child) {
+                parent.getSubItems().remove(child);
+            }
+        }
+    }
+
+    public int getParentPositionInAll(int position) {
+        List<T> data = getData();
+        MultiItemEntity multiItemEntity = getItem(position);
+
+        if (isExpandable(multiItemEntity)) {
+            IExpandable IExpandable = (com.z_martin.common.common.adapterHelper.entity.IExpandable) multiItemEntity;
+            for (int i = position - 1; i >= 0; i--) {
+                MultiItemEntity entity = data.get(i);
+                if (isExpandable(entity) && IExpandable.getLevel() > ((com.z_martin.common.common.adapterHelper.entity.IExpandable) entity).getLevel()) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = position - 1; i >= 0; i--) {
+                MultiItemEntity entity = data.get(i);
+                if (isExpandable(entity)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public boolean isExpandable(MultiItemEntity item) {
+        return item != null && item instanceof IExpandable;
+    }
+}
+
+
