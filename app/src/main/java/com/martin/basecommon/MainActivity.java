20a5e2af437ae50f138b3eaa0d87f5ebae1f2564
MartinQAQ <xiaoming723@126.com>
2020-06-09 22:18:33 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/martin/basecommon/MainActivity.java
@@ -0,0 +1,16 @@
+package com.martin.basecommon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.z_martin.common.base.activity.BaseActivity;
+import com.z_martin.common.base.annotations.ContentView;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        
+    }
+}
