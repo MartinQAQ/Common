ec7aafadce4b2dee3f4a504557d261fcac0b091f
MartinQAQ <xiaoming723@126.com>
2020-06-09 22:20:35 +0800

    Create MultiItemEntity.java

---
+++ Common/src/main/java/com/z_martin/common/common/adapterHelper/entity/MultiItemEntity.java
@@ -0,0 +1,8 @@
+package com.z_martin.common.common.adapterHelper.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+    int getItemType();
+}
