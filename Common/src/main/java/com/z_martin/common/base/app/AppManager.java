1f7392754f6479f3b077b876f61f5979732db0ea
MartinQAQ <xiaoming723@126.com>
2020-06-09 22:19:56 +0800

    Create AppManager.java

---
+++ Common/src/main/java/com/z_martin/common/base/app/AppManager.java
@@ -0,0 +1,115 @@
+package com.z_martin.common.base.app;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.Stack;
+
+public class AppManager {
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    private AppManager() {
+    }
+
+    public static AppManager getAppManager() {
+        if (instance == null) {
+            synchronized (AppManager.class) {
+                if (instance == null) {
+                    instance = new AppManager();
+                    instance.activityStack = new Stack();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(activity);
+    }
+
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
+
+    public void finishActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    public void finishActivity(Activity activity) {
+        if (activity != null) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    public void finishActivity(Class<?> cls) {
+        try {
+            for (Activity activity : activityStack) {
+                if (activity.getClass().equals(cls)) {
+                    finishActivity(activity);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void finishAllActivity() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+    public boolean isOpenActivity(Class<?> cls) {
+        if (activityStack != null) {
+            for (int i = 0, size = activityStack.size(); i < size; i++) {
+                if (cls == activityStack.peek().getClass()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void AppExit(Context context, Boolean isBackground) {
+        try {
+            finishAllActivity();
+            ActivityManager activityMgr =
+                    (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            activityMgr.killBackgroundProcesses(context.getPackageName());
+            System.exit(0);
+        } catch (Exception e) {
+        } finally {
+            if (!isBackground) {
+                System.exit(0);
+            }
+        }
+    }
+
+    public Activity getActivity(Class cls) {
+        if (activityStack == null || activityStack.size() == 0 || cls == null) {
+            return null;
+        }
+        for (Activity activity : activityStack) {
+            if (activity != null && activity.getClass() == cls) {
+                return activity;
+            }
+        }
+        return null;
+    }
+
+    public boolean isAppExit() {
+        return activityStack == null || activityStack.isEmpty();
+    }
+}
